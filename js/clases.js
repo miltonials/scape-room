f6548ef43c75571b69e2421bd45fd524814196e2
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2022-11-11 11:41:01 -0600

    Function of fitness
    based on Elliot Sober (2001)

--- js/clases.js
+++ js/clases.js
@@ -18,19 +18,62 @@ class Individual{
         return this.ADN
     }
 
-    // funcion para obtenerFinest
     // funcion para obtenerFitness
-    getFinest(){
-        return this.finest
+    getFitness(){
         return this.fitness
     }
 
-    // funcion para calcular Finest
-    // funcion para calcular Fitness
-    calcularFinest(){
-        this.finest = this.distancia + this.pasos
-        
+    /**
+     * method that returns how close is the individual to the goal
+     * @param {int} dimentions | dimentions of the matrix
+     * @returns {int} closing to the goal
+     */
+    calculateClosing(dimentions){
+        let x = this.axisX
+        let y = this.axisY
+
+        //the goal must be in coordinates (dimentions-1, dimentions-1)
+
+        let closeX = Math.abs(dimentions-1 - x)
+        let closeY = Math.abs(dimentions-1 - y)
+        let steps = 0
+
+        //how much steps in x the individual needs to take for reaching the goal
+        while(closeX <= dimentions-1){
+            steps += 1
+            closeX++
+        }
+
+        //how much steps in y the individual needs to take for reaching the goal
+        while(closeY <= dimentions-1){
+            steps += 1
+            closeY++
+        }
+
+        return steps
     }
+
+    /**
+     * method that calculates the fitness of an individual
+     * Sober, E. (2001)
+     * fitness (W) = survival * fecundity
+     * survival = distance (how long it takes to reach the goal)
+     * fecundity = steps (how much the indivual has walked)
+     * set percentage of fitness to individual 
+     * @param {int} dimentions | dimentions of the matrix
+     */
+    calculateFitness(dimentions){
+        this.nextStep()//in case of not being called it before
+        let w = 0
+        let distance = this.distancia
+        let steps = this.calculateClosing(dimentions)
+
+        w = distance*steps
+
+        //setting fitness
+        this.fitness = w/100
+    }
+
     nextStep() {
         let movi = ["W" ,"A","S","D"]
         let moviX = [0, -1, 0, 1]
